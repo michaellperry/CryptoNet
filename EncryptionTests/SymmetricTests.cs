9aba0c33cf8b8b67d53808cd7d88d798da10c84c
Michael L Perry <michael@qedcode.com>
2014-02-24 20:49:40 -0600

    Symmetric encryption unit tests.

--- EncryptionTests/SymmetricTests.cs
+++ EncryptionTests/SymmetricTests.cs
@@ -1,14 +1,108 @@
-﻿using System;
+﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace EncryptionTests
 {
     [TestClass]
-    public class UnitTest1
+    public class SymmetricTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GenerateARandomAESKey()
         {
+            SymmetricAlgorithm aes = new AesCryptoServiceProvider();
+            aes.KeySize = 256;
+
+            aes.Key.Length.Should().Be(32);     // 256 / 8
+            aes.IV.Length.Should().Be(16);      // 128 / 8
+        }
+
+        [TestMethod]
+        public void EncryptAMessageWithAES()
+        {
+            string message = "Alice knows Bob's secret.";
+            SymmetricAlgorithm aes = new AesCryptoServiceProvider();
+            aes.KeySize = 256;
+
+            var key = aes.Key;
+            var iv = aes.IV;
+
+            byte[] encryptedMessage = EncryptWithAes(message, aes);
+
+            string decryptedMessage = DecryptMessageWithAes(
+                key, iv, encryptedMessage);
+
+            decryptedMessage.Should().Be(message);
+        }
+
+        [TestMethod]
+        public void UsingTheSameInitializationVector()
+        {
+            string message1 = "Alice knows Bob's secret.";
+            string message2 = "Alice knows Bob's favorite color.";
+            SymmetricAlgorithm aes = new AesCryptoServiceProvider();
+            aes.KeySize = 256;
+
+            byte[] encryptedMessage1 = EncryptWithAes(message1, aes);
+            byte[] encryptedMessage2 = EncryptWithAes(message2, aes);
+
+            Enumerable.SequenceEqual(
+                encryptedMessage1.Take(16),
+                encryptedMessage2.Take(16))
+                .Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void UsingADifferentInitializationVector()
+        {
+            string message1 = "Alice knows Bob's secret.";
+            string message2 = "Alice knows Bob's favorite color.";
+            SymmetricAlgorithm aes = new AesCryptoServiceProvider();
+            aes.KeySize = 256;
+
+            byte[] key = aes.Key;
+            byte[] encryptedMessage1 = EncryptWithAes(message1, aes);
+            aes = new AesCryptoServiceProvider();
+            aes.Key = key;
+            byte[] encryptedMessage2 = EncryptWithAes(message2, aes);
+
+            Enumerable.SequenceEqual(
+                encryptedMessage1.Take(16),
+                encryptedMessage2.Take(16))
+                .Should().BeFalse();
+        }
+
+        private static byte[] EncryptWithAes(
+            string message, SymmetricAlgorithm aes)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            using (StreamWriter cryptoWriter = new StreamWriter(
+                new CryptoStream(
+                    memoryStream,
+                    aes.CreateEncryptor(),
+                    CryptoStreamMode.Write)))
+            {
+                cryptoWriter.Write(message);
+            }
+
+            return memoryStream.ToArray();
+        }
+
+        private static string DecryptMessageWithAes(
+            byte[] key, byte[] iv, byte[] encryptedMessage)
+        {
+            SymmetricAlgorithm provider = new AesCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream(encryptedMessage);
+            using (StreamReader cryptoReader = new StreamReader(
+                new CryptoStream(
+                    memoryStream,
+                    provider.CreateDecryptor(key, iv),
+                    CryptoStreamMode.Read)))
+            {
+                return cryptoReader.ReadToEnd();
+            }
         }
     }
 }
