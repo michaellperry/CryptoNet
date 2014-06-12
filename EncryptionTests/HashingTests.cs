6f3fbafe202bd9363edaa37a548dc551f4b9da3c
Michael L Perry <MichaelPerry@GameStop.com>
2014-06-12 10:06:39 -0500

    Added hashing tests.

---
+++ EncryptionTests/HashingTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography;
+using System.IO;
+using FluentAssertions;
+
+namespace EncryptionTests
+{
+    [TestClass]
+    public class HashingTests
+    {
+        [TestMethod]
+        public void HashFunctionIsDeterministic()
+        {
+            string password = "pass@word1";
+            byte[] hash1 = HashPassword(password);
+            byte[] hash2 = HashPassword(password);
+            
+            Convert.ToBase64String(hash2).Should().Be(
+                Convert.ToBase64String(hash1));
+        }
+
+        private static byte[] HashPassword(string password)
+        {
+            var memory = new MemoryStream();
+            using (var writer = new StreamWriter(memory))
+            {
+                writer.Write(password);
+            }
+            byte[] bytes = memory.ToArray();
+
+            throw new NotImplementedException();
+        }
+    }
+}
