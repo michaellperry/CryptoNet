a09495e458f8ba6cfee8460702008cb68f5db63a
Michael L Perry <michael@qedcode.com>
2014-06-12 22:30:28 -0500

    Added test for password based key derivation function.

--- EncryptionTests/HashingTests.cs
+++ EncryptionTests/HashingTests.cs
@@ -31,5 +31,40 @@ namespace EncryptionTests
 
             throw new NotImplementedException();
         }
+
+        [TestMethod]
+        public void PasswordBasedKeyDerivationFunctionIsSlower()
+        {
+            string password = "pass@word1";
+            byte[] hash1 = PBKDF(password);
+            byte[] hash2 = PBKDF(password);
+
+            Convert.ToBase64String(hash1).Should().Be(
+                Convert.ToBase64String(hash2));
+        }
+
+        private static byte[] PBKDF(string password)
+        {
+            var hashAlgorithm = new SHA256CryptoServiceProvider();
+
+            byte[] stage = HashPassword(password);
+            byte[] hash = new byte[stage.Length];
+            Array.Copy(stage, hash, stage.Length);
+
+            for (int round = 0; round < 100000; ++round)
+            {
+                stage = hashAlgorithm.ComputeHash(stage);
+                XorBytes(hash, stage);
+            }
+            return hash;
+        }
+
+        private static void XorBytes(byte[] target, byte[] source)
+        {
+            for (int index = 0; index < target.Length; index++)
+            {
+                target[index] ^= source[index];
+            }
+        }
     }
 }
